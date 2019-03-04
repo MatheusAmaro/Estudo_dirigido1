259f2cf797bd0d59ea17fef7b689f46d16cbce25
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-03-04 11:11:21 -0300

    Update Atividade4.php

--- Atividade4.php
+++ Atividade4.php
@@ -1,3 +1,16 @@
 <?php 
 
+print "Solicite o preço da mercadoria :";
+print "O preço é : ";
+
+$valor_produto= "int fgets(DNIT)";
+$desconto_produto(p_atual,desc):;
+$valor_desconto=(p_atual*desc)/100;
+$novo_preco=p_atual-valor_desconto;
+
+return (novo_preco,valor_desconto);
+
+p_atual=eval(input("Digite o valor do produto :"))
+desc=eval(input("Digite o valor do desconto :"))
+print("O novo valor do produto e do desconto é : ",desconto_produto(p_atual,desc))
 
