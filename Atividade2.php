834af4eaebd9754900ee50e802f4939698fa2914
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-03-04 09:07:18 -0300

    Update Atividade2.php
    Base para atividade do guilouro

--- Atividade2.php
+++ Atividade2.php
@@ -1,2 +1,12 @@
 <?php
 
+print "Leia a quantidade de dias , horas, minutos e segundos e depois calcule";
+
+$dias= input (Digite a quantidade de dias :);
+$horas= input (Digite a quantidade de horas :);
+$minutos= input (Digite a quantidade de minutos :);
+$segundos= input (Digite a quantidade de segundos :);
+
+$totalcalculado = (dias*24*60*60) + (horas*60*60) + (minutos*60) + segundos
+
+print dias,"$dias",horas,"$horas",minutos,"$minutos",segundos,"segundos representam", totalcalculado,"$segundos")
