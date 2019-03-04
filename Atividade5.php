74641b3495863d29f61add537d2a82fd2f741478
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-03-04 11:43:46 -0300

    Update Atividade5.php

--- Atividade5.php
+++ Atividade5.php
@@ -1,1 +1,10 @@
 <?php
+
+print "Calcule o tempo de viagem de um carro :";
+
+$dist= float (input(Digite a distancia em Km :));
+$velo= float (input(Digite a velocidade do carro:));
+
+tempo= dist/velo
+
+print ("Tempo em horas %.1f' %tempo");
