c40e2697c8e89bb3eadfe0c08a3c98bef8543081
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-03-04 09:40:18 -0300

    Create Atividade3.php

---
+++ Atividade3.php
@@ -0,0 +1,13 @@
+<?php
+
+print"Escreva o valor do sálario :";
+
+$salario= input (Digite o valor: ) ;
+
+$aumento= input (Digite o valor: );
+
+$nsalario= (aumento/100.0 * salario) + salario
+$valor_aumento = nsalario - salario
+
+print "\n Novo salario: ",$nsalario
+print "Aumento: ",#valor_aumento
