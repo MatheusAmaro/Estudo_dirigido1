53dfe014efdf11a206d2d3fc8e09c5e962935bc2
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-03-08 14:58:29 -0300

    Update Atividade2.php

--- Atividade2.php
+++ Atividade2.php
@@ -2,11 +2,20 @@
 
 print "Leia a quantidade de dias , horas, minutos e segundos e depois calcule";
 
-$dias= input (Digite a quantidade de dias :);
-$horas= input (Digite a quantidade de horas :);
-$minutos= input (Digite a quantidade de minutos :);
-$segundos= input (Digite a quantidade de segundos :);
+print"Digite a quantidade de dias :";
+$dias = (int) fgets (STDIN); 
+print"Digite a quantidade de horas :";
+$horas = (int) fgets (STDIN);
+print"Digite a quantidade de minutos :";
+$minutos = (int) fgets (STDIN);
+print"Digite a quantidade de segundos";
+$segundos = (int) fgets (STDIN);
 
-$totalcalculado = (dias*24*60*60) + (horas*60*60) + (minutos*60) + segundos
+$totalcalculado = (dias*24*60*60) + (horas*60*60) + (minutos*60) + (segundos*1);
 
-print dias,"$dias",horas,"$horas",minutos,"$minutos",segundos,"segundos representam", totalcalculado,"$segundos")
+print "O total de dias escrito foi $dias ";
+print "A quantidade de horas escrita foi $horas "; 
+print" A quantidade de minutos foi $minutos "; 
+print" A quantidade de segundos foi $segundos ";
+
+print"Depois de calcula o resultado foi $totalcalculado";
