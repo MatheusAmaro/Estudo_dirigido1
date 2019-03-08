cea8345fb36eba391290d1fbd256b8984aad9a6c
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-03-08 14:10:55 -0300

    Update Atividade8.php

--- Atividade8.php
+++ Atividade8.php
@@ -1,1 +1,14 @@
 <?php
+
+print "Quantos cigarros você fuma por dia?";
+$cigarros_fumados = (int) fgets (STDIN);
+
+print "A quantos anos você fuma?";
+$anos_fumados = (int) fgets (STDIN);
+
+$total_cigarros = ($anos_fumados*365)*$cigarros_fumados;
+$dias_perdidos = ($total_cigarros*10)/24;
+
+
+print "Você já perdeu $dias_perdidos dias de vida";
+
