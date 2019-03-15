463e8753f73957d3319820d9c4c0f18bb516a270
Matheus Amaro Maciel <48133433+MatheusAmaro@users.noreply.github.com>
2019-03-15 14:07:10 -0300

    Update Atividade6.php

--- Atividade6.php
+++ Atividade6.php
@@ -1,8 +1,11 @@
 <?php
 
-def conversaoCparaF(celsius):
-fahrenheit=9*celsius/5+32
-return fahrenheit
+print "Converta Celsius para Fahrenheit";
+print "Escreva qualquer Temperatura em Celsius :";
 
-celsius=eval(input(" Digite a temperatura em Celsius :"))
-print("O valor da conversão de Celsius para Fahrenheit foi :",conversaoCparaF(celsius))
+$C= (int) fgets (STDIN);
+print "Agora converta Celsius para Fahrenheit :";
+$F= 9*$C/5 + 32;
+
+print "A temperatura convertida para Fahrenheit foi : $F";
+
